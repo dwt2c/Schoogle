29e94b4be18f59b8aec0b40b9e3c4a258c3598e3
Adam25T <ajturner@owu.edu>
2015-12-16 02:34:42 -0500

    Drop table
    Drops all tables in our db if we ever need to start over run this

---
+++ DROP_TABLES.sql
@@ -0,0 +1,17 @@
+
+
+
+drop table USERS1 cascade;
+drop table USERS2 cascade;
+drop table CLIETN cascade;
+drop table SEARCHABLE cascade;
+drop table ANALYTICS cascade;
+drop table CRAWL_LOG cascade;
+drop table CRAWLERS cascade;
+drop table SEARCHES cascade;
+drop table ENGINES cascade;
+drop table DOMAIN cascade;
+drop table FULL_PAGE cascade;
+drop table CREATE_LOG_LOOKUP cascade;
+drop table IN1 cascade;
+drop table CONSUME cascade;
